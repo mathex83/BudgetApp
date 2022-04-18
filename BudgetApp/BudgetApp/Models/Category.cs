e82a3023ff6d66338a3c0a69abc5e8e95e18caa0
Martin <marnor83@gmail.com>
2022-04-18 17:03:34 +0200

    Category model, controller and view

---
+++ BudgetApp/BudgetApp/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BudgetApp.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Display(Name ="Kategori")]
+        public string CategoryName { get; set; }
+
+
+        public ICollection<Subcategory> Subcategories { get; set; }
+    }
+}
