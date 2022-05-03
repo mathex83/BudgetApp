eac992c1da414cc2997e4310a86c35612e8682bc
Martin <marnor83@gmail.com>
2022-05-03 18:29:11 +0200

    Create Subcategory.cs

---
+++ BudgetApp/BudgetApp/Models/CategoryModels/Subcategory.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace BudgetApp.Models.CategoryModels
+{
+    public class Subcategory
+    {
+        [Key]
+        public int SubcategoryId { get; set; }
+
+        public string SubcategoryName { get; set; }
+
+
+        #region SubCategoryCategoryId Foreign-end
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        #endregion
+
+        #region TransSubCategoryId Primary-end
+        public ICollection<Trans> Trans { get; set; }
+        #endregion
+    }
+}
