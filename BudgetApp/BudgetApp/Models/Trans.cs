5d6b74e9dab001167239b270a5c3e8eb3d17ddcc
Martin <marnor83@gmail.com>
2022-05-03 18:29:21 +0200

    Create Trans.cs

---
+++ BudgetApp/BudgetApp/Models/Trans.cs
@@ -0,0 +1,26 @@
+﻿using BudgetApp.Models.CategoryModels;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BudgetApp.Models
+{
+    public class Trans
+    {
+        [Key]
+        public int TransId { get; set; }
+        public string Description { get; set; }
+
+        [Column(TypeName = "decimal(10, 2)")]
+        public decimal Amount { get; set; }
+
+        #region TransAccountId Foreign-end
+        public int AccountId { get; set; }
+        public Account Account { get; set; }
+        #endregion
+
+        #region TransSubCategoryId Foreign-end
+        public int SubCategoryId { get; set; }
+        public Subcategory Subcategory { get; set; }
+        #endregion
+    }
+}
