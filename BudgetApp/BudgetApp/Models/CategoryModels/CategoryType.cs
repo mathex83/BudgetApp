30bb4886835c876ad66a963442602e416caba9e8
Martin <marnor83@gmail.com>
2022-05-03 18:29:08 +0200

    Create CategoryType.cs

---
+++ BudgetApp/BudgetApp/Models/CategoryModels/CategoryType.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BudgetApp.Models.CategoryModels
+{
+    public class CategoryType
+    {
+        [Key]
+        public int CategoryTypeId { get; set; }
+        public string CategoryTypeName { get; set; }
+
+        #region CategoryCategoryTypeId Primary-end
+        public ICollection<Category> Categories { get; set; }
+        #endregion
+    }
+}
