c55783933114eb4c1f847eb5b9aa7ac091a3aecc
Martin <marnor83@gmail.com>
2022-05-03 08:13:50 +0200

    Components for loading csv-file

---
+++ BudgetApp/BudgetApp/Controllers/CSV_modelController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BudgetApp.Data;
+using BudgetApp.Models.CSV_loader;
+
+namespace BudgetApp.Controllers
+{
+    public class CSV_modelController : Controller
+    {
+        private readonly BudgetAppContext _context;
+
+        public CSV_modelController(BudgetAppContext context)
+        {
+            _context = context;
+        }
+
+        // GET: CSV_model
+        public IActionResult CSVlist()
+        {
+            FileUpload fu = new FileUpload();
+            return View(fu.lines);
+        }
+
+        private bool CSV_modelExists(int id)
+        {
+            return _context.CSV_model.Any(e => e.LineId == id);
+        }
+    }
+}
---
+++ BudgetApp/BudgetApp/Models/CSV-loader/FileUpload.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace BudgetApp.Models.CSV_loader
+{
+    public class FileUpload
+    {
+        public string FileUrl { get; set; } = @"wwwroot/csv/Budget 2021-01-01t2022-05-02.csv";
+        public List<CSV_model> lines = new List<CSV_model>();
+        public FileUpload()
+        {
+            string[] fromFile = File.ReadAllLines(FileUrl);
+            for (int i = 1; i < fromFile.Length; i++)
+            {
+                string[] something = fromFile[i].Split(';');
+                lines.Add(new CSV_model
+                {
+                    Bogføringsdato = something[0],
+                    Beløb = Convert.ToDecimal(something[1]),
+                    Afsender = something[2],
+                    Modtager = something[3],
+                    Navn = something[4],
+                    Beskrivelse = something[5],
+                    Saldo = Convert.ToDecimal(something[6]),
+                    Valuta = something[7],
+                    Afstemt = something[8],
+                });
+            }
+        }
+    }
+}
