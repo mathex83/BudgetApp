280d6a9c93525238e03c948dd98fca83142761b8
Martin <marnor83@gmail.com>
2022-05-29 10:50:46 +0200

    Update FileUpload.cs

--- BudgetApp/BudgetApp/Models/CSV-loader/FileUpload.cs
+++ BudgetApp/BudgetApp/Models/CSV-loader/FileUpload.cs
@@ -7,25 +7,36 @@ namespace BudgetApp.Models.CSV_loader
     public class FileUpload
     {
         public string FileUrl { get; set; } = @"wwwroot/csv/Budget 2021-01-01t2022-05-02.csv";
-        public List<CSV_model> lines = new List<CSV_model>();
+        public List<CSV_model> fileLines = new();
         public FileUpload()
         {
+            string unwantedSubstring = "bs betaling ";
             string[] fromFile = File.ReadAllLines(FileUrl);
             for (int i = 1; i < fromFile.Length; i++)
             {
-                string[] something = fromFile[i].Split(';');
-                lines.Add(new CSV_model
+                string[] lineSplit = fromFile[i].Split(';');
+                if (lineSplit.Length == 9)
                 {
-                    Bogføringsdato = something[0],
-                    Beløb = Convert.ToDecimal(something[1]),
-                    Afsender = something[2],
-                    Modtager = something[3],
-                    Navn = something[4],
-                    Beskrivelse = something[5],
-                    Saldo = Convert.ToDecimal(something[6]),
-                    Valuta = something[7],
-                    Afstemt = something[8],
-                });
+                    CSV_model model = new()
+                    {
+                        TransactionDate = lineSplit[0],
+                        Amount = Convert.ToDecimal(lineSplit[1]),
+                        SendingAccount = lineSplit[2],
+                        ReceivingAccount = lineSplit[3],
+                        NameOfTransaction = lineSplit[4],
+                        Description = lineSplit[5],
+                        Saldo = Convert.ToDecimal(lineSplit[6]),
+                        Currency = lineSplit[7],
+                        TransactionApproved = lineSplit[8]
+                    };
+                    fileLines.Add(model);
+                    if (model.Description.ToLower().StartsWith(unwantedSubstring))
+                    {
+                        model.Description = model.Description.Substring(
+                            unwantedSubstring.Length,
+                            model.Description.Length - unwantedSubstring.Length);
+                    }
+                }                
             }
         }
     }
