1d62983dceabf94959feeff6f4b06c13c556ade0
Martin <marnor83@gmail.com>
2022-07-04 11:25:50 +0200

    Update FileUpload.cs

--- BudgetApp/BudgetApp/Models/CSV-loader/FileUpload.cs
+++ BudgetApp/BudgetApp/Models/CSV-loader/FileUpload.cs
@@ -6,7 +6,7 @@ namespace BudgetApp.Models.CSV_loader
 {
     public class FileUpload
     {
-        public string FileUrl { get; set; } = @"wwwroot/csv/Budget 2021-01-01t2022-05-02.csv";
+        private string FileUrl { get; set; } = @"wwwroot/csv/Budget 2021-01-01t2022-05-02.csv";
         public List<CSV_model> fileLines = new();
         public FileUpload()
         {
@@ -23,11 +23,11 @@ namespace BudgetApp.Models.CSV_loader
                         Amount = Convert.ToDecimal(lineSplit[1]),
                         SendingAccount = lineSplit[2],
                         ReceivingAccount = lineSplit[3],
-                        NameOfTransaction = lineSplit[4],
                         Description = lineSplit[5],
                         Saldo = Convert.ToDecimal(lineSplit[6]),
                         Currency = lineSplit[7],
-                        TransactionApproved = lineSplit[8]
+                        TransactionApproved = lineSplit[8],
+                        NameOfTransaction = ""
                     };
                     fileLines.Add(model);
                     if (model.Description.ToLower().StartsWith(unwantedSubstring))
@@ -36,8 +36,34 @@ namespace BudgetApp.Models.CSV_loader
                             unwantedSubstring.Length,
                             model.Description.Length - unwantedSubstring.Length);
                     }
-                }                
+                }
             }
+            foreach (CSV_model c in fileLines)
+            {
+                c.NameOfTransaction = SetTransactionName(c.Description);
+            }
+        }
+        private string SetTransactionName(string model)
+        {
+            if (model.ToLower().Contains("ele")) return "El";
+            if (model.ToLower().Contains("norlys")) return "El";
+            if (model.ToLower().Contains("leje")) return "Husleje";
+            if (model.ToLower().Contains("alka")) return "Bilforsikring";
+            if (model.ToLower().Contains("moto")) return "Grøn afgift";
+            if (model.ToLower().Contains("hiper")) return "Internet";
+            if (model.ToLower().Contains("call")) return "Mobil";
+            if (model.ToLower().Contains("cbb")) return "Mobil";
+            if (model.ToLower().Contains("telia")) return "Mobil";
+            if (model.ToLower().Contains("netflix")) return "Netflix";
+            if (model.ToLower().Contains("viaplay")) return "Viaplay";
+            if (model.ToLower().Contains("disney")) return "Disney+";
+            if (model.ToLower().Contains("prosa")) return "Fagforening og A-kasse";
+            if (model.ToLower().Contains("liv")) return "Forsikring gruppeliv";
+            if (model.ToLower().Contains("lb")) return "Forsikring indbo og ulykke";
+            if (model.ToLower().Contains("sygef")) return "Sygeforsikring Danmark";
+            if (model.ToLower().Contains("budget")) return "Budget";
+            if (model.ToLower().Contains("licens")) return "Licens";
+            return "";
         }
     }
 }
